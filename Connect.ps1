7f7663bd999f22e6d0e01d199eeb1e54cd508956
unknown <dennis.alonzo.johnson@gmail.com>
2019-06-28 07:26:11 -0400

    START

---
+++ Connect.ps1
@@ -0,0 +1,52 @@
+﻿connect-azaccount
+
+$context = Get-AzSubscription | Select id
+
+Set-AzContext -subscriptionid 
+
+$rgs = Get-AzResourceGroup | select ResourcegroupName
+
+write-output $rgs[1]
+
+function Show-Menu
+{
+    param (
+    [string]$Title = 'my menu'
+    )
+    Clear-host
+    Write-Host "====================================="
+    Write-Host "1: Press 1" $rgs[0]
+    Write-Host "2: Press 1" $rgs[1]
+    Write-Host "3: Press 1" $rgs[2]
+    Write-Host "4: Press 1" $rgs[3]
+    Write-Host "q to quit"
+
+
+}
+
+
+do
+{
+
+    Show-Menu -Title 'my menu'
+
+    $selection = Read-Host "Please make a selection"
+    switch ($selection)
+    {
+        '1'{
+        " picked 1"
+        $rsgrp = $rgs[0]
+        }'2'{
+        " picked 2"
+        $rsgrp = $rgs[1]
+        }'3'{
+        "picked 3"
+        $rsgrp = $rgs[2]
+        }
+        }
+        pause
+}
+until ($selection -eq 'q')
+
+
+$rsgrp
---
+++ Untitled3.ps1
@@ -0,0 +1,8 @@
+﻿$services = Get-Service -first 5
+$menu = @{}
+for ($i=1;$i -le $services.count; $i++) 
+{ Write-Host "$i. $($services[$i-1].name),$($services[$i-1].status)" 
+$menu.Add($i,($services[$i-1].name))}
+
+[int]$ans = Read-Host 'Enter selection'
+$selection = $menu.Item($ans) ; Get-Service $selection
